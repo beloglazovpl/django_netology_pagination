3e0c7c30f84e18b03c840f05adba60fa98bd3eec
Pavel Beloglazov <beloglazov_pl@icloud.com>
2022-02-20 23:01:50 +1000

    Update view.py

--- stations/views.py
+++ stations/views.py
@@ -5,6 +5,11 @@ from pagination.settings import BUS_STATION_CSV
 import csv
 
 
+with open(BUS_STATION_CSV) as file:
+    reader = csv.DictReader(file)
+    stations = [row for row in reader]
+
+
 def index(request):
     return redirect(reverse('bus_stations'))
 
@@ -13,14 +18,10 @@ def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
     page_number = int(request.GET.get('page', 1))
-    with open(BUS_STATION_CSV) as file:
-        reader = csv.DictReader(file)
-        stations = [row for row in reader]
-
     paginator = Paginator(stations, 10)
     page = paginator.get_page(page_number)
     context = {
-        'bus_stations': page,
+        'bus_stations': page.object_list,
         'page': page,
     }
     return render(request, 'stations/index.html', context)
